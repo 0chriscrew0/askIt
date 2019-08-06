316010192a9597ebeabb50f20831105738bd43de
Christopher Crow <0chriscrew0@gmail.com>
2019-08-06 12:11:21 -0500

    setup project for deployment

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const graphqlHttp = require("express-graphql");
 const mongoose = require("mongoose");
-require('dotenv').config();
+const async = require("async");
+const path = require("path");
+require("dotenv").config();
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolvers = require("./graphql/resolvers");
@@ -39,12 +41,17 @@ const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${
   process.env.MONGO_PASSWORD
 }@cluster0-vqy2w.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 // Create Mongo connection
 mongoose
-  .connect(
-    mongoURI,
-    { useNewUrlParser: true }
-  )
+  .connect(mongoURI, { useNewUrlParser: true })
   .then(() => {
     const port = 5000 || process.env.PORT;
     app.listen(port, () => console.log(`Server started on port ${port}`));
